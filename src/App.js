520c38202084801adefadc661f5e38210e9b9b00
Nicola Martinez <nicoenrique_1994@hotmail.com>
2021-08-23 22:09:45 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [tarea, setTarea] = useState([]);
+  const [listareas, setListareas] = useState('');
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    setTarea({
+      tarea: [...tarea, listareas],
+      listareas: ''
+    })
+  }
+  const handleChange = ({ target: { value } }) => setListareas({
+    listareas: value
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo List</h1>
+      <div className="row">
+        <form onSubmit={onSubmit}>
+          <div className="col-md-8 mx-auto" >
+            <div className="input-group">
+              <input type="text" className="form-control" aria-label="Text input with radio button" onChange={handleChange} placeholder="Actividad a agregar" />
+              <button className="btn  btn-primary mx-auto" type="submit">Add to list!</button>
+              {
+                tarea.map((li, key) => <li {...{ key }}>{li}</li>)
+              }
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
